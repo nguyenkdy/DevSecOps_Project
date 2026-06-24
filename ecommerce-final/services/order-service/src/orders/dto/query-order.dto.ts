cc6d2fe3c8014b0a7dfa0374bc71bdb142157b19
Duy Nguyễn Khánh <nguyenkhanhduy20050616@gmail.com>
2026-06-24 14:45:05 +0700

    fixed

--- ecommerce-final/services/order-service/src/orders/dto/query-order.dto.ts
+++ ecommerce-final/services/order-service/src/orders/dto/query-order.dto.ts
@@ -1,15 +1,18 @@
 import { IsOptional, IsInt, Min, Max } from 'class-validator';
+import { Type } from 'class-transformer';
 
 /**
  * Query parameters cho list orders
  */
 export class QueryOrderDto {
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number = 1;
 
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   @Max(100)
