32b183753497d3268a1ed8681ba73fa7443030df
Duy Nguyễn Khánh <nguyenkhanhduy20050616@gmail.com>
2026-06-25 13:26:13 +0700

    fix: allow underscore-prefixed unused args in api-gateway eslint

--- services/api-gateway/.eslintrc.js
+++ services/api-gateway/.eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
   },
 };
