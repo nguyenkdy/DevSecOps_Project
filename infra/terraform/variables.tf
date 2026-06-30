dc4afccf282e35405fcaaaacc31bd251203a257e
Duy Nguyễn Khánh <nguyenkhanhduy20050616@gmail.com>
2026-06-30 11:36:18 +0700

    fixed version EKS

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project_name" {
 variable "eks_cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.33"
+  default     = "1.35"
 }
 
 variable "eks_node_instance_types" {
